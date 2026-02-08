c340d8abcceed8f6294466192560f19c0e4cb1d0
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2026-02-08 09:15:06 +0200

    refactor

--- manifest/backtest.py
+++ manifest/backtest.py
@@ -89,6 +89,10 @@ def backtest_dca(
         index=dates,
     )
 
+    result['Averege_dominance'] = result['Price'] >= result['Avg_price']
+    bull_history = result['Averege_dominance'].value_counts(normalize=True).iloc[0]
+    bull_history = int(bull_history * 100)
+
     metrics = {
         "Cash_spent": int(cash_spent),
         "Final_portfolio_value": int(portfolio_value[-1]),
@@ -96,94 +100,95 @@ def backtest_dca(
         "Total_returns": int(returns_sum),
         "Total_equity": int(portfolio_value[-1]) + int(returns_sum),
         "Num_take_profits": len(trigger_dates),
+        "Bull_history": bull_history
     }
 
-    result['Averege_dominance'] = result['Price'] >= result['Avg_price']
-    bull_history = result['Averege_dominance'].value_counts(normalize=True).iloc[0]
-    bull_history = int(bull_history * 100)
-
     if plot:
-        fig, ax_price = plt.subplots(figsize=(10, 5))
-
-        # --- ціна активу ---
-        ax_price.plot(
-            result.index,
-            result["Price"],
-            color="orange",
-            label="Asset price",
-            linewidth=1.0,
-        )
-
-        # --- середня ціна ---
-        ax_price.plot(
-            result.index,
-            result["Avg_price"],
-            color="red",
-            linestyle="--",
-            linewidth=1.0,
-            alpha=0.5,
-            label="Average price",
-        )
-
-        ax_price.set_ylabel("Asset price ($)")
-        ax_price.set_xlabel("Date")
-
-        # --- тейк-профіти ---
-        if trigger_dates:
-            ax_price.scatter(
-                trigger_dates,
-                result.loc[trigger_dates]["Price"],
-                color="red",
-                marker="o",
-                s=20,
-                zorder=5,
-                label="Take-profit",
-            )
-
-        for dt in trigger_dates:
-            profit = result.loc[dt]["Realized_profit"]
-            price = result.loc[dt]["Price"]
-
-            ax_price.annotate(
-                f"{profit:.0f}$",
-                xy=(dt, price),
-                xytext=(0, 8),
-                textcoords="offset points",
-                ha="right",
-                fontsize=12,
-                color="black",
-            )
-
-        # --- портфель ---
-        ax_portfolio = ax_price.twinx()
-        ax_portfolio.plot(
-            result.index,
-            result["Portfolio"],
-            label="Portfolio ($)",
-            alpha=0.5,
-        )
-
-        # --- вкладення ---
-        invested_plot = result["Invested"].resample("ME").last()
-        ax_portfolio.bar(
-            invested_plot.index,
-            invested_plot.values,
-            width=20,
-            alpha=0.1,
-            label="Invested capital ($)",
-        )
-
-        ax_portfolio.set_ylabel("Portfolio / Invested ($)")
-
-        # --- легенда ---
-        l1, lab1 = ax_price.get_legend_handles_labels()
-        l2, lab2 = ax_portfolio.get_legend_handles_labels()
-        ax_price.legend(l1 + l2, lab1 + lab2, loc="upper left")
-
-        plt.title(f"{target} : Cash_spent: {metrics['Cash_spent']}, Final_portfolio_value: {metrics['Final_portfolio_value']}, Bull_history: {bull_history}%," +
-                  f"\n Realized_profit: {metrics['Realized_profit']}, Total_returns: {metrics['Total_returns']}, Total_equity: {metrics['Total_equity']}, Num_take_profits: {metrics['Num_take_profits']}")
-        
-        plt.tight_layout()
-        plt.show()
+        plot_graph(target, metrics, result, trigger_dates)
 
     return result, metrics
+
+
+def plot_graph(target, metrics, result, trigger_dates):
+    fig, ax_price = plt.subplots(figsize=(10, 5))
+
+    # --- ціна активу ---
+    ax_price.plot(
+        result.index,
+        result["Price"],
+        color="orange",
+        label="Asset price",
+        linewidth=1.0,
+    )
+
+    # --- середня ціна ---
+    ax_price.plot(
+        result.index,
+        result["Avg_price"],
+        color="red",
+        linestyle="--",
+        linewidth=1.0,
+        alpha=0.5,
+        label="Average price",
+    )
+
+    ax_price.set_ylabel("Asset price ($)")
+    ax_price.set_xlabel("Date")
+
+    # --- тейк-профіти ---
+    if trigger_dates:
+        ax_price.scatter(
+            trigger_dates,
+            result.loc[trigger_dates]["Price"],
+            color="red",
+            marker="o",
+            s=20,
+            zorder=5,
+            label="Take-profit",
+        )
+
+    for dt in trigger_dates:
+        profit = result.loc[dt]["Realized_profit"]
+        price = result.loc[dt]["Price"]
+
+        ax_price.annotate(
+            f"{profit:.0f}$",
+            xy=(dt, price),
+            xytext=(0, 8),
+            textcoords="offset points",
+            ha="right",
+            fontsize=12,
+            color="black",
+        )
+
+    # --- портфель ---
+    ax_portfolio = ax_price.twinx()
+    ax_portfolio.plot(
+        result.index,
+        result["Portfolio"],
+        label="Portfolio ($)",
+        alpha=0.5,
+    )
+
+    # --- вкладення ---
+    invested_plot = result["Invested"].resample("ME").last()
+    ax_portfolio.bar(
+        invested_plot.index,
+        invested_plot.values,
+        width=20,
+        alpha=0.1,
+        label="Invested capital ($)",
+    )
+
+    ax_portfolio.set_ylabel("Portfolio / Invested ($)")
+
+    # --- легенда ---
+    l1, lab1 = ax_price.get_legend_handles_labels()
+    l2, lab2 = ax_portfolio.get_legend_handles_labels()
+    ax_price.legend(l1 + l2, lab1 + lab2, loc="upper left")
+
+    plt.title(f"{target} : Cash_spent: {metrics['Cash_spent']}, Final_portfolio_value: {metrics['Final_portfolio_value']}, Bull_history: {metrics['Bull_history']}%," +
+                f"\n Realized_profit: {metrics['Realized_profit']}, Total_returns: {metrics['Total_returns']}, Total_equity: {metrics['Total_equity']}, Num_take_profits: {metrics['Num_take_profits']}")
+    
+    plt.tight_layout()
+    plt.show()
