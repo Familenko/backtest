4f3e712b75a81640b904f98314c22c30319e2545
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2026-02-15 15:26:30 +0200

    correct loading func and fix TON tickers in config

--- manifest/config.py
+++ manifest/config.py
@@ -1,21 +1,21 @@
 from datetime import datetime
 
 
-START_DATE = "2026-02-10"
+START_DATE = "2025-02-13"
 TODAY = datetime.now().strftime("%Y-%m-%d")
 
 # crypto
-crypto = {
-    "BTC": "BTC-USD",
-    "ETH": "ETH-USD",
-    "BNB": "BNB-USD",
-    "XRP": "XRP-USD",
-    "SOL": "SOL-USD",
-    "DOT": "DOT-USD",
-    "ADA": "ADA-USD",
-    "TON": "TON-USD",
-    "LINK": "LINK-USD",
-    "AVAX": "AVAX-USD"
+CRYPTO = {
+    "BTC": {"tickers": "BTC-USD", "start_date": START_DATE, "end_date": TODAY},
+    "ETH": {"tickers": "ETH-USD", "start_date": START_DATE, "end_date": TODAY},
+    "BNB": {"tickers": "BNB-USD", "start_date": START_DATE, "end_date": TODAY},
+    "XRP": {"tickers": "XRP-USD", "start_date": START_DATE, "end_date": TODAY},
+    "SOL": {"tickers": "SOL-USD", "start_date": START_DATE, "end_date": TODAY},
+    "DOT": {"tickers": "DOT-USD", "start_date": START_DATE, "end_date": TODAY},
+    "ADA": {"tickers": "ADA-USD", "start_date": START_DATE, "end_date": TODAY},
+    "TON": {"tickers": "TON11419-USD", "start_date": START_DATE, "end_date": TODAY},
+    "LINK": {"tickers": "LINK-USD", "start_date": START_DATE, "end_date": TODAY},
+    "AVAX": {"tickers": "AVAX-USD", "start_date": START_DATE, "end_date": TODAY}
 }
 crypto_regular_amount = 10
 crypto_freq = "W-MON"
@@ -24,9 +24,9 @@ crypto_profit_multiple = 2
 crypto_cooldown_days = 180
 
 # stocks
-stocks = [
-    'SPY'
-]
+STOCKS = {
+    'SPY': {"tickers": "SPY", "start_date": START_DATE, "end_date": TODAY}
+}
 stock_regular_amount = 300
 stock_freq = "WOM-3THU"
 stock_fee = 0.01
--- manifest/load_data.py
+++ manifest/load_data.py
@@ -1,26 +1,23 @@
 import yfinance as yf
 import pandas as pd
+import time
 
-from config import START_DATE, TODAY, stocks, crypto
+from config import STOCKS, CRYPTO
 
 
-def load_data(tickers, start_date=START_DATE, end_date=TODAY):
-    print(f"Loading data for {tickers} from {start_date} to {end_date}...")
-    if isinstance(tickers, dict):
-        df = pd.DataFrame()
-        for k, v in tickers.items():
-            df[k] = yf.download(v, start=start_date, end=end_date, auto_adjust=False)["Close"]
-    else:
-        df = pd.DataFrame()
-        for ticker in tickers:
-            df[ticker] = yf.download(ticker, start=start_date, end=end_date, auto_adjust=False)["Close"]
+def load_data(tickers):
+    df = pd.DataFrame()
+    for k, v in tickers.items():
+        print(f"Loading data for {k} from {v['start_date']} to {v['end_date']}...")
+        df[k] = yf.download(v["tickers"], start=v["start_date"], end=v["end_date"], auto_adjust=False)["Close"]
+        time.sleep(1)
 
     return df
 
 
 if __name__ == "__main__":
-    df_stocks = load_data(stocks)
-    df_crypto = load_data(crypto)
+    df_stocks = load_data(STOCKS)
+    df_crypto = load_data(CRYPTO)
     
     df_stocks.to_csv("data/stocks.csv")
     df_crypto.to_csv("data/crypto.csv")
